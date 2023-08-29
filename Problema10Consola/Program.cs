475b4bcc25da012ea82dc60ff50bdc30c38aca7b
OMAR <OMAR@FIORELA>
2023-08-28 22:43:03 -0500

    Agregar archivos de proyecto.

---
+++ Problema10Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Ingresa el consumo :");
+double consumo=double.Parse(Console.ReadLine());
+double descuento = 0;
+
+if (consumo <= 100)
+{
+    descuento = consumo * 0.10;
+
+}
+else
+{
+    descuento = consumo * 0.20;
+
+}
+
+double totalconsumo = consumo - descuento;
+double impuesto = totalconsumo * 0.18;
+double total = totalconsumo + impuesto;
+
+Console.WriteLine("total descuento: " + totalconsumo+ " soles" );
+Console.WriteLine("el impuesto es: " + impuesto+ " soles");
+Console.WriteLine("esto es el total que pagara : "+total+ " soles");
